6ced35d2f9e377b8e33ee2e97f153b42547a3666
Samantha Gonzales <samagonz@fiu.edu>
2023-11-30 12:43:33 -0500

    fix typo

--- analysis/consensus-causal-graph/human/scripts/2_generate_struct_config_human.R
+++ analysis/consensus-causal-graph/human/scripts/2_generate_struct_config_human.R
@@ -18,4 +18,4 @@ generateStructConfig(configFile = structmb.config.out, data = data,
                              DataFile = sub("\\.", "/home/samanthagonzales/zinc_aging", data.in),
                              Order = best.order,
                              OutputFileName = paste0("/home/samanthagonzales/zinc_aging/consensus-causal-graph/", organism, "/structures/out/", tolower(dataset), "_struct_results.txt"))
-cat("Structures configuration file can be found in", struct.config.out)
+cat("Structures configuration file can be found in", structmb.config.out)
