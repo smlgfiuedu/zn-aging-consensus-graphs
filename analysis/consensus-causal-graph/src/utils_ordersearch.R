dbb3b2f5fae7baf25ca5c6a5305f8ad80203d794
Samantha Gonzales <samagonz@fiu.edu>
2023-11-30 10:58:02 -0500

    add function to generate structmb config

--- analysis/consensus-causal-graph/src/utils_ordersearch.R
+++ analysis/consensus-causal-graph/src/utils_ordersearch.R
@@ -41,3 +41,34 @@ generatePrepriorConfig <- function(configFile = "preprior.config",
   close(fw)
   return(config_opts)
 }
+
+
+# Generate config file for GetStructure.cpp -------------------------------
+generateStructConfig <- function(configFile = "structmb.config",
+                                 data = NA, DataFile = "", TotalVariables = ncol(data), 
+                                 MaximumCategory = apply(data, 2, max), MinimumCategory = apply(data, 2, min),
+                                 MaximumParents = 4, Order = 0:(ncol(data)-1), Percentage = "99.0",
+                                 OutputFileName = "output.structures.txt"){
+  
+  cat("Creating config file for", basename(DataFile), "\n")
+  config_opts <- list(DataFile = DataFile,
+                      TotalVariables = TotalVariables,
+                      MaximumCategory = paste(MaximumCategory, collapse = " "),
+                      MinimumCategory = paste(MinimumCategory, collapse = " "),
+                      MaximumParents = MaximumParents,
+                      Order = paste(Order, collapse = " "),
+                      Percentage = Percentage,
+                      OutputFileName = OutputFileName)
+  
+  fw <- file(configFile, open = "wt")
+  
+  for (c in 1:length(config_opts)){
+    
+    line <- paste(names(config_opts)[c], "=", config_opts[[c]])
+    writeLines(line, fw)
+  }
+  close(fw)
+  return(config_opts)
+}
+
+
